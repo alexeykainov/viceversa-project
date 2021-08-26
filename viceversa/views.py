4a32cc1922c05864a582c13e464b806b353440ad
alexeykainov <pochta_alexeya@mail.ru>
2021-08-26 22:40:14 +0300

    Modify app

--- viceversa/views.py
+++ viceversa/views.py
@@ -8,4 +8,9 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['usertext']
 	reversed_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text})
+	num_words = len(user_text.split())
+	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text, 
+		'num_origin_words':num_words})
+
+
+
